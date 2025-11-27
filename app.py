f152ea95a3761909119002e3b507cded5b30b1e5
RAB025 <84223482+RAB025@users.noreply.github.com>
2025-11-27 18:33:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>🚀 Hello from Azure Flask App!</h1><p>Your Flask app is successfully deployed on Azure App Service 🎉</p>"
+    return "<h1>🚀 Hello there from Azure Flask App! made a few changes.</h1><p>Your Flask app is successfully deployed on Azure App Service 🎉</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
